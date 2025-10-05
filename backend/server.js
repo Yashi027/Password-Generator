67a71d8c50cdf4d3555386ca5441acfa546587a8
Yashi Bansal <bansalyashi163@gmail.com>
2025-10-05 19:04:56 +0530

    setting up env for deploying

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,24 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const vaultRoutes = require('./routes/vault');
-
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/auth', authRoutes);
+app.use('/vault', vaultRoutes);
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
-
-app.use('/auth', authRoutes);
-app.use('/vault', vaultRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
